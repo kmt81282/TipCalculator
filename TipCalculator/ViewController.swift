f5ae2f0d54c2d189b255d73686da4790d431489e
marcturner <kturner54@student.gsu.edu>
2023-03-12 15:08:26 -0400

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Marc Turner on 3/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
